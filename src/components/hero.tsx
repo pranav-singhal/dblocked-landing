44684f3f71517ad7e0c5c4d2ac8e8f90dc7cf653
Pranav Singhal <pranavsinghal96@gmail.com>
2024-07-08 03:30:08 +0530

    fixed logo

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,25 +1,24 @@
 'use client'  
-import { ArrowRight } from "lucide-react";
 import WaitlistForm from "./ui/form/WaitlistForm";
 
 export function Hero() {
   
   return (
     <section className="container flex flex-col items-center gap-8 pt-20 sm:gap-10">
-      
-      <div className="bg-secondary hover:bg-secondary/60 flex cursor-pointer items-center gap-1 rounded-full border px-3 py-0.5">
-        <span className="text-secondary-foreground text-sm">Join &quot;0 to web3&quot; today!</span>
-        <ArrowRight size={16} />
+
+      <div className="flex cursor-pointer items-center gap-1 rounded-full border bg-secondary px-3 py-0.5 hover:bg-secondary/60">
+        <span className="text-sm text-secondary-foreground">Join &quot;0 to web3&quot; today!</span>
+        {/* <ArrowRight size={16} /> */}
       </div>
-      <h1 className="font-heading max-w-3xl text-center text-4xl font-semibold sm:text-5xl sm:leading-tight">
+      <h1 className="max-w-3xl text-center font-heading text-4xl font-semibold sm:text-5xl sm:leading-tight">
         Become a Fullstack Web3 Developer
       </h1>
-      <p className="text-muted-foreground max-w-lg text-center text-lg sm:text-xl">
+      <p className="max-w-lg text-center text-lg text-muted-foreground sm:text-xl">
         Get access to a free, continuous developer education program that grows and evolves with the
         industry
       </p>
      <WaitlistForm />
-      <p className="text-muted-foreground max-w-lg text-center text-lg sm:text-xl">
+      <p className="max-w-lg text-center text-lg text-muted-foreground sm:text-xl">
         Watch the intro video 👇
       </p>
       <div className="h-96 w-full md:px-48 ">
@@ -41,7 +40,7 @@ export function Hero() {
           priority
           className="rounded-xl border border-border shadow-lg"
         /> */}
-        <div className="bg-primary/20 absolute inset-0 -z-10 [filter:blur(180px)]" />
+        <div className="absolute inset-0 -z-10 bg-primary/20 [filter:blur(180px)]" />
       </div>
     </section>
   );
