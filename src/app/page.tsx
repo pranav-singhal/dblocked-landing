0b71c070cb7b4f5d63544faaf0ae5097d16c572e
Pranav Singhal <pranavsinghal96@gmail.com>
2024-07-13 00:49:28 +0530

    added metatags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,18 @@ import { Hero } from "@/components/hero";
 import { SocialProof } from "@/components/social-proof";
 import { Technologies } from "@/components/technologies";
 import { Testimonials } from "@/components/testimonials";
+import type { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'DBlockEd | 0 To Web3',
+  description: '0 to Web3 is a continuous developer education program. It is a never ending program that makes you a full stack web3 developer',
+  openGraph: {
+    images: ['/images/banner.jpg']
+  },
+  twitter: {
+    images: ['/images/banner.jpg']
+  },
+}
 
 export default function Home() {
   return (
