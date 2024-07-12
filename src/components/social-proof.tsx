c090e1993bdd191f895243dfac948d93493f7e27
Pranav Singhal <pranavsinghal96@gmail.com>
2024-07-13 01:14:34 +0530

    added classroomIO section

--- src/components/social-proof.tsx
+++ src/components/social-proof.tsx
@@ -6,12 +6,15 @@ export function SocialProof() {
       <h2 className="text-center text-lg font-semibold leading-8">
         Supported by
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-2">
         
-      <div className="col-span-1 md:col-start-2">
+      <div className="col-span-1 ">
+        <Image src="/images/ethindia-grants.png" alt="EthIndia Grants" width={10000} height={1000} className="object-contain" />
+      </div>
 
-          <Image src="/images/ethindia-grants.png" alt="Company Logo" width={10000} height={1000} className="object-contain" />
-        </div>
+      <div className="col-span-1">
+        <Image src="/images/classroomio.png" alt="ClassromIO" width={10000} height={1000} className="h-full object-contain align-middle" />
+      </div>
       </div>
     </section>
   );
